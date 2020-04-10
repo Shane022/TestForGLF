50473f036311055687b510a0badeee21e6b33c59
shangzhixin <1013956388@qq.com>
2020-04-10 09:37:41 +0800

    Initial Commit

---
+++ TestForGLF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestForGLF
+//
+//  Created by Shane on 2020/4/10.
+//  Copyright © 2020 Shane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TestForGLF/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  TestForGLF
+//
+//  Created by Shane on 2020/4/10.
+//  Copyright © 2020 Shane. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
